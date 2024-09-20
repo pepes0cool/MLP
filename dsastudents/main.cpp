66633c6ea060953313e42a2f6847b98f6d2357ba
Pepes0cool <115146605+phucmap10@users.noreply.github.com>
2024-09-21 00:09:29 +0700

    Update main.cpp

--- dsastudents/main.cpp
+++ dsastudents/main.cpp
@@ -3,12 +3,26 @@
 #include <sstream>
 #include <string>
 #include "list/listheader.h"
-
 using namespace std;
-
-
+#include "ann/funtions.h"
+#include "ann/xtensor_lib.h"
+#include "ann/dataset.h"
+#include "ann/dataloader.h"
+#include "list/DLinkedList.h"
+#include "list/DLinkedListDemo.h"
+#include "util/Point.h"
+#include "list/IList.h"
 int main(int argc, char** argv) {
-    cout << "Assignment-1" << endl;
+    int nsamples = 80;
+    xt::xarray<double> X = xt::random::randn<double>({nsamples, 10});
+    xt::xarray<double> T = xt::random::randn<double>({nsamples, 5});
+    TensorDataset<double, double> ds(X, T);
+    DataLoader<double, double> loader(&ds, 30, true, false);
+    for (auto& batch : loader) {
+        cout << "Data shape: " << xt::adapt(batch.getData().shape()) << endl;
+        cout << "Label shape: " << xt::adapt(batch.getLabel().shape()) << endl;
+    }
+
     return 0;
 }
 
