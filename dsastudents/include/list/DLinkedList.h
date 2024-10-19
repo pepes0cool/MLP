10de5d966a84b0454014e4be77bc66f80b8f9a71
Pepes0cool <115146605+phucmap10@users.noreply.github.com>
2024-10-19 23:47:07 +0700

    Update DLinkedList.h

--- dsastudents/include/list/DLinkedList.h
+++ dsastudents/include/list/DLinkedList.h
@@ -442,19 +442,7 @@ template <class T>
 void DLinkedList<T>::clear()
 {
     // TODO
-    if (deleteUserData != nullptr) {
     removeInternalData();
-        deleteUserData(this);
-    }
-    Node* curr = head->next;
-    while(curr != tail){
-        Node* tmp = curr;
-        curr = curr->next;
-        delete tmp;
-    }
-    head->next = tail;
-    tail->prev = head;
-    count = 0;
 }
 
 template <class T>
